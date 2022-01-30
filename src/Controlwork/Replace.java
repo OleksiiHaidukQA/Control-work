4342d35557cfb3867446a5788cc9651ac96f3a12
Oleksii Haiduk <aleksgayduk@gmail.com>
2022-01-30 23:32:12 +0200

    Control work

---
+++ src/Controlwork/Replace.java
@@ -0,0 +1,14 @@
+package Controlwork;
+
+public class Replace {
+    public void replace() {
+        String stringWithRandomChars = "Hello control work. Try to replace";
+
+        String finalString = stringWithRandomChars
+                .replace("a", "@")
+                .replace("o", "0");
+
+        System.out.println(finalString);
+
+    }
+}
---
+++ src/Controlwork/Reverse.java
@@ -0,0 +1,24 @@
+package Controlwork;
+
+public class Reverse {
+        public void reverse() {
+
+            int num = 1234, reversed = 0;
+
+            System.out.println("Original Number: " + num);
+
+
+            while(num != 0) {
+
+
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+
+
+                num /= 10;
+            }
+
+            System.out.println("Reversed Number: " + reversed);
+        }
+    }
+
